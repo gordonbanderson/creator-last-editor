519eeeadb383cf7c3d48bf1967821d72f757e8e0
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-01-21 01:24:11 +0700

    MINOR: Convert to PSR2

--- code/CreatorLastEditorExtension.php
+++ code/CreatorLastEditorExtension.php
@@ -1,33 +1,34 @@
-<?php 
-class CreatorLastEditorExtension extends DataExtension {
-	private static $has_one = array(
-		'Creator' => 'Member',
-		'LastEditor' => 'Member'
-	);
+<?php
 
-	/*
-	1) Save the creator as the current editing member if there is not creator already assigned
-	2) Save the last editor (e.g. an admin) as the person who last edited this document
-	*/
-	public function onBeforeWrite() {
-		if ($this->owner->CreatorID == 0) {
-			$this->owner->CreatorID = Member::currentUserID();
-		}
-		$this->owner->LastEditorID = Member::currentUserID();
-	}
+class CreatorLastEditorExtension extends DataExtension
+{
+    private static $has_one = array(
+        'Creator' => 'Member',
+        'LastEditor' => 'Member',
+    );
 
+    /*
+    1) Save the creator as the current editing member if there is not creator already assigned
+    2) Save the last editor (e.g. an admin) as the person who last edited this document
+    */
+    public function onBeforeWrite()
+    {
+        if ($this->owner->CreatorID == 0) {
+            $this->owner->CreatorID = Member::currentUserID();
+        }
+        $this->owner->LastEditorID = Member::currentUserID();
+    }
 
-	/*
-	Allow the admin to override the creator and last editor
-	*/
-	public function updateCMSFields(FieldList $fields) {
+    /*
+    Allow the admin to override the creator and last editor
+    */
+    public function updateCMSFields(FieldList $fields)
+    {
+        if (Permission::check('ADMIN')) {
+            $memberField = new DropdownField('CreatorID', 'Creator', Member::get()->map('ID', 'Username', '-- Please select --'));
+            $fields->addFieldToTab('Root.Creator', $memberField);
+        }
 
-	    if (Permission::check('ADMIN')) {
-	    	$memberField  = new DropdownField('CreatorID', 'Creator', Member::get()->map('ID', 'Username', '-- Please select --'));
-	    	$fields->addFieldToTab( "Root.Creator", $memberField );
-	    }
-	    
-	    return $fields;
-	 }
-
+        return $fields;
+    }
 }
--- code/CreatorOnlyCanEditFormExtension.php
+++ code/CreatorOnlyCanEditFormExtension.php
@@ -1,23 +1,26 @@
-<?php 
-class CreatorOnlyCanEditFormExtension extends Extension {
-	function updateEditForm(&$form) {
-		//error_log('Creator id:'.$this->owner->CreatorID);
-		//error_log('Current member id:'.Member::currentUserID());
+<?php
 
-		$creatorID = Member::currentUserID();
-		$formfields = $form->fields;
-		$formid = $formfields->fieldbyName('ID');
-		//error_log($formid->value());
-		$sql = "select CreatorID from SiteTree where ID=".$formid->value().' Limit 1';
-		$result = DB::query($sql);
-		$first = $result->first();
-		$creatorID = $first['CreatorID'];
-		//error_log('CREATOR: '.$creatorID);
+class CreatorOnlyCanEditFormExtension extends Extension
+{
+    public function updateEditForm(&$form)
+    {
+        //error_log('Creator id:'.$this->owner->CreatorID);
+        //error_log('Current member id:'.Member::currentUserID());
 
-		if (!(Permission::check('ADMIN'))) {
-			if ($creatorID != Member::currentUserID()) {
-				$form->makeReadonly();
-			}
-		}
-	}
+        $creatorID = Member::currentUserID();
+        $formfields = $form->fields;
+        $formid = $formfields->fieldbyName('ID');
+        //error_log($formid->value());
+        $sql = 'select CreatorID from SiteTree where ID='.$formid->value().' Limit 1';
+        $result = DB::query($sql);
+        $first = $result->first();
+        $creatorID = $first['CreatorID'];
+        //error_log('CREATOR: '.$creatorID);
+
+        if (!(Permission::check('ADMIN'))) {
+            if ($creatorID != Member::currentUserID()) {
+                $form->makeReadonly();
+            }
+        }
+    }
 }
